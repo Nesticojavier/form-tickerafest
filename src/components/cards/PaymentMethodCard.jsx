bf534bbb4cd0a0fd85eb8a330cd10dc7e81a0c60
nestorjavier <nestor06sep@gmail.com>
2023-12-14 13:14:45 -0400

    added a unique key prop to payment method card

--- src/components/cards/PaymentMethodCard.jsx
+++ src/components/cards/PaymentMethodCard.jsx
@@ -4,8 +4,8 @@ const PaymentMethodCard = ({method}) => {
   return (
     <div className="mt-4 rounded-lg border border-gray-200 bg-white shadow-sm">
       <dl className="space-y-6 border-gray-200 px-4 py-6 sm:px-6">
-        {method.map((data) => (
-          <div className="flex items-center justify-between">
+        {method.map((data, index) => (
+          <div key={index} className="flex items-center justify-between">
             <dt className="text-md font-bold">{data.name}</dt>
             <dd className="text-md font-medium text-black">{data.value}</dd>
           </div>
