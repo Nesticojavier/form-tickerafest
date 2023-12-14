05e43bd8f0d1bec7392bafac522040a0f82c9e20
nestorjavier <nestor06sep@gmail.com>
2023-12-14 11:08:35 -0400

    remove border-t cards

--- src/components/cards/Transfer.jsx
+++ src/components/cards/Transfer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Transfer = () => {
   return (
     <div className="mt-4 rounded-lg border border-gray-200 bg-white shadow-sm">
-      <dl className="space-y-6 border-t border-gray-200 px-4 py-6 sm:px-6">
+      <dl className="space-y-6 border-gray-200 px-4 py-6 sm:px-6">
         <div className="flex items-center justify-between">
           <dt className="text-sm">Banco</dt>
           <dd className="text-sm font-medium text-gray-900">Banco Mercantil</dd>
